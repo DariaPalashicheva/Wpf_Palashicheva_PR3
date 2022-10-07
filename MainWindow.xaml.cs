fd0d5089349d7f91221211549944eb19af0bde7d
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-10-07 12:49:00 +1000

    Стили

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Wpf_Palashicheva_PR3
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button is clicked");
+        }
     }
 }
